58cd97482f3d620505b3d7338a8e7b385c451210
Fankux <fankux@gmail.com>
2020-07-20 16:08:08 +0800

    Disable RocksDB compression libraries auto detecting & Fix Protobuf zlib dep (#99)
    * rocksdb use cmake compile && fix protobuf zlib dep
    
    * baikal-client dep
    
    * binary output path

--- cmake/rocksdb.cmake
+++ cmake/rocksdb.cmake
@@ -19,9 +19,11 @@ SET(ROCKSDB_INSTALL_DIR ${THIRD_PARTY_PATH}/install/rocksdb)
 SET(ROCKSDB_INCLUDE_DIR "${ROCKSDB_INSTALL_DIR}/include" CACHE PATH "rocksdb include directory." FORCE)
 SET(ROCKSDB_LIBRARIES "${ROCKSDB_INSTALL_DIR}/lib/librocksdb.a" CACHE FILEPATH "rocksdb library." FORCE)
 
-FILE(WRITE ${ROCKSDB_SOURCES_DIR}/src/build.sh
-        "PORTABLE=1 make -j${NUM_OF_PROCESSOR} static_lib"
-        )
+set(prefix_path "${THIRD_PARTY_PATH}/install/snappy|${THIRD_PARTY_PATH}/install/zlib|${THIRD_PARTY_PATH}/install/gflags")
+
+#FILE(WRITE ${ROCKSDB_SOURCES_DIR}/src/build.sh
+#        "PORTABLE=1 make -j${NUM_OF_PROCESSOR} static_lib"
+#        )
 
 ExternalProject_Add(
         extern_rocksdb
@@ -32,10 +34,34 @@ ExternalProject_Add(
 #        GIT_TAG "v6.8.1"
         URL "https://github.com/facebook/rocksdb/archive/v6.8.1.tar.gz"
         UPDATE_COMMAND ""
-        CONFIGURE_COMMAND ""
-        BUILD_IN_SOURCE 1
-        BUILD_COMMAND mv ../build.sh . COMMAND sh build.sh
-        INSTALL_COMMAND mkdir -p ${ROCKSDB_INSTALL_DIR}/lib COMMAND cp -r include ${ROCKSDB_INSTALL_DIR}/ COMMAND cp librocksdb.a ${ROCKSDB_LIBRARIES}
+#        CONFIGURE_COMMAND ""
+#        BUILD_IN_SOURCE 1
+#        BUILD_COMMAND mv ../build.sh . COMMAND sh build.sh
+#        INSTALL_COMMAND mkdir -p ${ROCKSDB_INSTALL_DIR}/lib COMMAND cp -r include ${ROCKSDB_INSTALL_DIR}/ COMMAND cp librocksdb.a ${ROCKSDB_LIBRARIES}
+        CMAKE_ARGS -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+        -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+        -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
+        -DCMAKE_CXX_FLAGS_RELEASE=${CMAKE_CXX_FLAGS_RELEASE}
+        -DCMAKE_CXX_FLAGS_DEBUG=${CMAKE_CXX_FLAGS_DEBUG}
+        -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
+        -DCMAKE_C_FLAGS_DEBUG=${CMAKE_C_FLAGS_DEBUG}
+        -DCMAKE_C_FLAGS_RELEASE=${CMAKE_C_FLAGS_RELEASE}
+        -DCMAKE_INSTALL_PREFIX=${ROCKSDB_INSTALL_DIR}
+        -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+        -DCMAKE_BUILD_TYPE=${THIRD_PARTY_BUILD_TYPE}
+        -DCMAKE_PREFIX_PATH=${prefix_path}
+        -DPORTABLE=ON
+        -DWITH_SNAPPY=ON
+        -DWITH_RUNTIME_DEBUG=ON
+        -DROCKSDB_BUILD_SHARED=OFF
+        -DWITH_BENCHMARK_TOOLS=OFF
+        -DWITH_CORE_TOOLS=OFF
+        -DWITH_TOOLS=OFF
+        ${EXTERNAL_OPTIONAL_ARGS}
+        LIST_SEPARATOR |
+        CMAKE_CACHE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=${ROCKSDB_INSTALL_DIR}
+        -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON
+        -DCMAKE_BUILD_TYPE:STRING=${THIRD_PARTY_BUILD_TYPE}
 )
 
 ADD_DEPENDENCIES(extern_rocksdb zlib snappy gflags)
