63b117c6b6ab6d189a0acee14480650322c597ed
liguoqiang <lgqss@126.com>
2022-08-29 16:46:34 +0800

    Delete src/tools/script/backup directory

--- src/tools/script/backup/meta_query_region_peers_status.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo -e "query region peers status\n"
-echo 'param: meta address'
-curl -d '{
-    "op_type" : "QUERY_REGION_PEER_STATUS"
-}' http://$1/MetaService/query
-echo -e "\n"
-
-
